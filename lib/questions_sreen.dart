786d46056ecef6e735b70d84a2558d9ca4b27679
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-09-27 09:06:04 +0200

    add questions file

--- lib/questions_sreen.dart
+++ lib/questions_sreen.dart
@@ -11,6 +11,36 @@ class QuestionsSreen extends StatefulWidget {
 class _QuestionsSreen extends State<QuestionsSreen> {
   @override
   Widget build(context) {
-    return const Text('Questions screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The Questions...',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Anser 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Anser 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Anser 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Anser 4'),
+          ),
+        ],
+      ),
+    );
   }
 }
