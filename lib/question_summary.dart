452f15f2b16d79c0fca26dc3df05801f68021809
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-09-30 12:18:01 +0200

    ad syle for question_summary

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -1,27 +1,73 @@
 import 'package:flutter/material.dart';
 
 class QuestionSummary extends StatelessWidget {
-  const QuestionSummary({super.key, required this.summaryData});
   final List<Map<String, Object>> summaryData;
+  const QuestionSummary({super.key, required this.summaryData});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map((el) {
-        return Row(
-          children: [
-            Text(((el['index'] as int) + 1).toString()),
-            Column(
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((el) {
+            return Row(
               children: [
-                Text((el['question'] as String)),
-                const SizedBox(height: 5),
-                Text((el['correct_ansquers'] as String)),
-                const SizedBox(height: 5),
-                Text((el['user_answer'] as String)),
+                Center(
+                  child: Container(
+                    width: 36,
+                    height: 36,
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle, // Imposta la forma circolare
+                      color: (el['is_correct'] as bool)
+                          ? Colors.green
+                          : Colors.red,
+                    ),
+                    child: Center(
+                      child: Text(((el['index'] as int) + 1).toString()),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 30,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        (el['question'] as String),
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 199, 199, 199),
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        (el['correct_answers'] as String),
+                        style: const TextStyle(
+                          color: Colors.green,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      if (el['user_answer'] != el['correct_answers'])
+                        Text(
+                          (el['user_answer'] as String),
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 201, 38, 12),
+                            fontSize: 16,
+                          ),
+                        ),
+                      const SizedBox(height: 33),
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ],
-        );
-      }).toList(),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -11,11 +11,13 @@ class ResultsScreen extends StatelessWidget {
     final List<Map<String, Object>> summary = [];
 
     for (var i = 0; i < choseAnsewers.length; i++) {
+      final bool isCorrect = choseAnsewers[i] == questions[i].answer[0];
       summary.add({
         'index': i,
         'question': questions[i].question,
-        'correct_ansquers': questions[i].answer[0],
+        'correct_answers': questions[i].answer[0],
         'user_answer': choseAnsewers[i],
+        'is_correct': isCorrect,
       });
     }
 
@@ -24,6 +26,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((el) {
+      return el['user_answer'] == el['correct_answers'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -31,14 +39,77 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answer X of Y question correctly!!!'),
+            Center(
+              child: Text.rich(
+                TextSpan(
+                  text: 'You answered ', // Parte normale
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 199, 199, 199),
+                    fontSize: 20.0,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '$numCorrectQuestions',
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 231, 22, 11),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const TextSpan(
+                      text: ' of ',
+                    ),
+                    TextSpan(
+                      text: '$numTotQuestions ',
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 11, 231, 51),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const TextSpan(
+                      text: 'questions correctly!!!',
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
             const SizedBox(
               height: 30,
             ),
-            QuestionSummary(summaryData: getSummaryData()),
+            QuestionSummary(summaryData: summaryData),
+            const SizedBox(
+              height: 33,
+            ),
             TextButton(
-              onPressed: () {},
-              child: const Text('Restart Quiz'),
+              onPressed: () {
+                // Aggiungi
+              },
+              style: TextButton.styleFrom(
+                side: const BorderSide(
+                  color: Color.fromARGB(255, 9, 240, 20),
+                  width: 2,
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18)),
+              ),
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.refresh,
+                    color: Color.fromARGB(255, 88, 222, 47),
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    'Restart Quiz',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 9, 240, 20),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
